51bb331cb96537e531d4f804f9ae6032f63c860c
rahul raul <rahul raul>
2021-03-27 20:43:23 +0530

    first commit

---
+++ src/main/java/com/example/spring/docker/docker/DockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.docker.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/docker/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.docker.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
